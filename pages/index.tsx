91810c9d320cc0e89d7978ee472ce0569d59824a
Quang Hung <hungpham307@gmail.com>
2023-02-24 10:36:27 +0700

    make Loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import Loader from "../components/Loader";
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-semibold underline">Welcome to dev note</h1>
-  );
+  return <Loader show />;
 }
